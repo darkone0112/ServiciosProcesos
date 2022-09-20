feaa0bd7ab1bc53b54ba3218665e800d23df3e88
darkone0112 <darkone0112@gmail.com>
2022-09-20 18:41:21 +0200

    desde Portatil

---
+++ holaPsp.java
@@ -0,0 +1,10 @@
+/**
+ * holaPsp
+ */
+public class holaPsp {
+    public static void main(String[]args) {
+        System.out.print("Hello World");
+        System.out.print("\n");
+        System.out.println("Compilado en Linux");
+    }
+}
